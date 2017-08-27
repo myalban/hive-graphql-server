8f2596334498e9b7249199492cdb37d57a81831c
Eric Typaldos <ettypaldos@gmail.com>
2017-08-26 21:15:21 -0400

    Add basic Meteor auth token check

--- src/mongo-connector.js
+++ src/mongo-connector.js
@@ -1,11 +1,12 @@
 import { MongoClient } from 'mongodb';
 
-const MONGO_URL = 'mongodb://localhost:27017/hackernews';
+// const MONGO_URL = 'mongodb://localhost:27017/hackernews';
+const MONGO_URL = 'mongodb://localhost:3001/meteor';
 
 module.exports = async () => {
   const db = await MongoClient.connect(MONGO_URL);
   return {
-    Links: db.collection('links'),
+    Links: db.collection('hnLinks'),
     Users: db.collection('users'),
     Votes: db.collection('votes'),
   };
