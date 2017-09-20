7ab034c535ff1a221625d1f3c31817ac657a6555
Max Shvets <max.shv@gmail.com>
2017-09-20 17:30:24 -0400

    Removed log

--- src/index.js
+++ src/index.js
@@ -52,7 +52,6 @@ const start = async () => {
   // since graphql only allows GET and POST requests.
   // NOTE: Is this correct? https://github.com/graphql/express-graphql/issues/14
   app.use('/graphql', (req, res, next) => {
-    console.log(req);
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, email');
     // console.log(req);
