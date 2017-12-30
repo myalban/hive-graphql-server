02d49f37871204e912e41e2f770b2fcb35a7caef
etyp <ettypaldos@gmail.com>
2017-12-30 00:01:37 -0500

    Lint

--- src/utils/format-error.js
+++ src/utils/format-error.js
@@ -3,7 +3,7 @@ import { formatError } from 'graphql';
 // Overwrites default error format but ensuring
 // the `field` custom value we used in our `ValidationError`
 // class is present when errors are sent to client.
-module.exports = error => {
+module.exports = (error) => {
   const data = formatError(error);
   const { originalError } = error;
   data.field = originalError && originalError.field;
