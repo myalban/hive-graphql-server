4ac0ebdc7861e42e2c8559b6327e407c5e7fecc5
etyp <ettypaldos@gmail.com>
2018-01-28 19:50:02 -0500

    Add subscriptions to resolvers

--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,13 +1,14 @@
 const { Query: ActionQuery, Mutation: ActionMutation, Action } = require('./action/resolvers');
 const { File } = require('./file/resolvers');
-const { Query: GroupQuery, Mutation: GroupMutation, Group } = require('./group/resolvers');
-const { Query: MessageQuery, Mutation: MessageMutation, Message } = require('./message/resolvers');
+const { Query: GroupQuery, Mutation: GroupMutation, Group, Subscription: GroupSubscription } = require('./group/resolvers');
+const { Query: MessageQuery, Mutation: MessageMutation, Message, Subscription: MessageSubscription } = require('./message/resolvers');
 const { Query: UserQuery, Mutation: UserMutation, User } = require('./user/resolvers');
 const { UserSettings } = require('./user-settings/resolvers');
 
 module.exports = {
   Query: Object.assign({}, ActionQuery, GroupQuery, MessageQuery, UserQuery),
   Mutation: Object.assign({}, ActionMutation, GroupMutation, MessageMutation, UserMutation),
+  Subscription: Object.assign({}, GroupSubscription, MessageSubscription),
   Action,
   File,
   Group,
