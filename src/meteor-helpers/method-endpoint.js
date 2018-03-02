223dfe86e6b1688460abce53935f59145059121b
etyp <ettypaldos@gmail.com>
2018-03-02 12:06:35 -0500

    Add better error messaging on method enpoint call

--- src/meteor-helpers/method-endpoint.js
+++ src/meteor-helpers/method-endpoint.js
@@ -1,13 +1,23 @@
 import request from 'request-promise';
+import environment from '../utils/environment-helpers';
+
+const protocol = environment.isDev() ? 'http' : 'https';
 
 export const callMethodAtEndpoint = async (methodName, headers = {}, methodArgs) => {
-  const url = `https://${process.env.METEOR_URL}/hvmethods/v1/`;
+  const url = `${protocol}://${process.env.METEOR_URL}/hvmethods/v1/`;
   const options = {
     method: 'POST',
     headers,
     url: url + methodName,
     json: methodArgs,
   };
-  const response = await request(options);
-  return response.data;
+  try {
+    const response = await request(options);
+    return response.data;
+  } catch (methodError) {
+    throw new Error(`
+      Error calling method ${methodName} at ${url}:
+      ${methodError}
+    `);
+  }
 };
